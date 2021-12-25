3c174e1c292fd5e3f73a3d93ed1b0c7a6cc17af3
Flamgop <firefaefly@gmail.com>
2021-12-25 13:50:16 -0800

    Remove Unnecessary code
    Removed extra `println`s as well as fixing the order of the buttons (I forgot 6)

--- src/main/java/com/mattworzala/debug/client/screens/DebugRenderersGUI.java
+++ src/main/java/com/mattworzala/debug/client/screens/DebugRenderersGUI.java
@@ -15,63 +15,59 @@ public class DebugRenderersGUI extends LightweightGuiDescription {
 
         WToggleButton button = new WToggleButton(new LiteralText("Pathfinders"));
         button.setToggle(com.mattworzala.debug.DebugRenderer.debugPathfinders);
-        button.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugPathfinders = bool;});
+        button.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugPathfinders = bool);
 
         WToggleButton button1 = new WToggleButton(new LiteralText("Water"));
         button1.setToggle(com.mattworzala.debug.DebugRenderer.debugWater);
-        button1.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugWater = bool;});
+        button1.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugWater = bool);
 
         WToggleButton button2 = new WToggleButton(new LiteralText("Height Maps"));
         button2.setToggle(com.mattworzala.debug.DebugRenderer.debugHeightMap);
-        button2.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugHeightMap = bool;});
+        button2.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugHeightMap = bool);
 
         WToggleButton button3 = new WToggleButton(new LiteralText("Collision"));
         button3.setToggle(com.mattworzala.debug.DebugRenderer.debugCollision);
-        button3.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugCollision = bool;});
+        button3.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugCollision = bool);
 
         WToggleButton button4 = new WToggleButton(new LiteralText("Neighbour Updates"));
         button4.setToggle(com.mattworzala.debug.DebugRenderer.debugNeighbourUpdate);
-        button4.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugNeighbourUpdate = bool;});
+        button4.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugNeighbourUpdate = bool);
 
         WToggleButton button5 = new WToggleButton(new LiteralText("Structures"));
         button5.setToggle(com.mattworzala.debug.DebugRenderer.debugStructure);
-        button5.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugStructure = bool;});
+        button5.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugStructure = bool);
 
-        WToggleButton button7 = new WToggleButton(new LiteralText("Sky Light"));
-        button7.setToggle(com.mattworzala.debug.DebugRenderer.debugSkyLight);
-        button7.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugSkyLight = bool;});
+        WToggleButton button6 = new WToggleButton(new LiteralText("Sky Light"));
+        button6.setToggle(com.mattworzala.debug.DebugRenderer.debugSkyLight);
+        button6.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugSkyLight = bool);
 
-        WToggleButton button8 = new WToggleButton(new LiteralText("World Gen"));
-        button8.setToggle(com.mattworzala.debug.DebugRenderer.debugWorldGen);
-        button8.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugWorldGen = bool;});
+        WToggleButton button7 = new WToggleButton(new LiteralText("World Gen"));
+        button7.setToggle(com.mattworzala.debug.DebugRenderer.debugWorldGen);
+        button7.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugWorldGen = bool);
 
-        WToggleButton button9 = new WToggleButton(new LiteralText("Block Outlines"));
-        button9.setToggle(com.mattworzala.debug.DebugRenderer.debugBlockOutline);
-        button9.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugBlockOutline = bool;});
+        WToggleButton button8 = new WToggleButton(new LiteralText("Block Outlines"));
+        button8.setToggle(com.mattworzala.debug.DebugRenderer.debugBlockOutline);
+        button8.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugBlockOutline = bool);
 
-        WToggleButton button10 = new WToggleButton(new LiteralText("Chunk Loading"));
-        button10.setToggle(com.mattworzala.debug.DebugRenderer.debugChunkLoading);
-        button10.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugChunkLoading = bool;});
+        WToggleButton button9 = new WToggleButton(new LiteralText("Chunk Loading"));
+        button9.setToggle(com.mattworzala.debug.DebugRenderer.debugChunkLoading);
+        button9.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugChunkLoading = bool);
 
-        WToggleButton button11 = new WToggleButton(new LiteralText("Villages"));
-        button11.setToggle(com.mattworzala.debug.DebugRenderer.debugVillage);
-        button11.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugVillage = bool;});
+        WToggleButton button10 = new WToggleButton(new LiteralText("Bees"));
+        button10.setToggle(com.mattworzala.debug.DebugRenderer.debugBee);
+        button10.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugBee = bool);
 
-        WToggleButton button12 = new WToggleButton(new LiteralText("Bees"));
-        button12.setToggle(com.mattworzala.debug.DebugRenderer.debugBee);
-        button12.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugBee = bool;});
+        WToggleButton button11 = new WToggleButton(new LiteralText("Raids"));
+        button10.setToggle(com.mattworzala.debug.DebugRenderer.debugRaid);
+        button10.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugRaid = bool);
 
-        WToggleButton button13 = new WToggleButton(new LiteralText("Raids"));
-        button13.setToggle(com.mattworzala.debug.DebugRenderer.debugRaid);
-        button13.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugRaid = bool;});
+        WToggleButton button12 = new WToggleButton(new LiteralText("Goal Selectors"));
+        button10.setToggle(com.mattworzala.debug.DebugRenderer.debugGoalSelector);
+        button10.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugGoalSelector = bool);
 
-        WToggleButton button14 = new WToggleButton(new LiteralText("Goal Selectors"));
-        button14.setToggle(com.mattworzala.debug.DebugRenderer.debugGoalSelector);
-        button14.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugGoalSelector = bool;});
-
-        WToggleButton button15 = new WToggleButton(new LiteralText("Game Events"));
-        button15.setToggle(com.mattworzala.debug.DebugRenderer.debugGameEvent);
-        button15.setOnToggle(bool -> { System.out.println("Debug: "+bool); com.mattworzala.debug.DebugRenderer.debugGameEvent = bool;});
+        WToggleButton button13 = new WToggleButton(new LiteralText("Game Events"));
+        button13.setToggle(com.mattworzala.debug.DebugRenderer.debugGameEvent);
+        button13.setOnToggle(bool -> com.mattworzala.debug.DebugRenderer.debugGameEvent = bool);
 
         root.add(button, 0, 1);
         root.add(button1, 0, 2);
@@ -86,8 +82,6 @@ public class DebugRenderersGUI extends LightweightGuiDescription {
         root.add(button11, 0, 11);
         root.add(button12, 0, 12);
         root.add(button13, 0, 13);
-        root.add(button14, 0, 14);
-        root.add(button15, 0, 15);
 
         root.validate(this);
     }
