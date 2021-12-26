7aa0aaf8eaf1e8ee2aa54c11d91c872e9e45aa8f
Flamgop <firefaefly@gmail.com>
2021-12-26 10:20:21 -0800

    Forgot LiteralText import :/

--- src/main/java/com/mattworzala/debug/DebugRenderer.java
+++ src/main/java/com/mattworzala/debug/DebugRenderer.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
 import org.lwjgl.glfw.GLFW;
 
 public class DebugRenderer implements ModInitializer {
